0d25eaa0457f8118de884930adbeeb5368aa396e
yylunxie <yylunxie@gmail.com>
2024-08-26 11:51:22 +0800

    temporarly delete about me

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,11 @@ import Footer from "./sections/Footer/Footer";
 import Hero from "./sections/Hero/Hero";
 import Projects from "./sections/Projects/Projects";
 import Skills from "./sections/Skills/Skills";
-import AboutMe from "./sections/AboutMe/AboutMe";
 
 function App() {
   return (
     <>
       <Hero />
-      <AboutMe />
       <Projects />
       <Skills />
       <Contact />
